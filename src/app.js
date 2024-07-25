6e1f8db25e02626723a71ed1124915e009cb4058
Daniel <69821353+DanielAcevedo99@users.noreply.github.com>
2024-07-25 15:42:41 -0500

    recommender 50%

--- src/app.js
+++ src/app.js
@@ -3,46 +3,41 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { fileURLToPath } from 'url';
-
+import path from 'path';
+import { FRONTEND_URL } from "./config.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import betsRoutes from "./routes/bets.routes.js";
 import apiSportsLiveScores from './routes/apiSportsLiveScores.routes.js';
-import apiSportsUpcomingRoutes from './routes/apiSportsUpcoming.routes.js'; // API ROUTE
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+import apiSportsUpcomingRoutes from './routes/apiSportsUpcoming.routes.js';
+import recommenderRoutes from './routes/recommenderRoutes.js';
 
-import { FRONTEND_URL } from "./config.js";
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: FRONTEND_URL,
-  })
-);
+app.use(cors({
+  credentials: true,
+  origin: FRONTEND_URL,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api", betsRoutes);
-app.use("/api",apiSportsLiveScores);
-app.use("/api",apiSportsUpcomingRoutes); // API ROUTE
+app.use("/api", apiSportsLiveScores);
+app.use("/api", apiSportsUpcomingRoutes);
+app.use("/api/recommender", recommenderRoutes); 
 
 if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html") );
     res.sendFile(path.resolve("client", "dist", "index.html"));
   });
 }
 
-export default app;
-
-// Route for serving HTML file
 app.get('/upcomingEvents', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'upcomingEvents.html'));
 });
@@ -50,3 +45,5 @@ app.get('/upcomingEvents', (req, res) => {
 app.get('/liveScores', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'liveScores.html'));
 });
+
+export default app;
